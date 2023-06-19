a7869879f0f71aca7547cc5cd9c77c9f3b34a290
Andy Repton <andy.repton@redhat.com>
2023-06-19 17:57:21 +0200

    Revert "Adding Rosa Cluster"
    This reverts commit f0a42d4290a2a03d5174208cd1c5f477f268a794.

--- variables.tf
+++ variables.tf
@@ -35,20 +35,6 @@ variable "openshift_version" {
   default = "4.12"
 }
 
-variable "availability_zones" {
-  type    = list(any)
-  default = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-}
-
-variable "aws_subnet_ids" {
-  type = list(any)
-}
-
-variable "replicas" {
-  type    = number
-  default = 3
-}
-
-variable "machine_cidr" {
-  type = string
-}
+#variable "installer_role_arn" {
+#  type = string
+#}
