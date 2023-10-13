01db4d70161e8f22f1ad415a2bebb2fcde5570ad
Andy Repton <andy.repton@redhat.com>
2023-10-13 10:27:55 +0200

    Start of new tf changes

--- account-roles.tf
+++ account-roles.tf
@@ -1,73 +1,68 @@
-# Gather all policies from OCM. If this fails, double check token
-data "ocm_policies" "all_policies" {}
+data "rhcs_policies" "all_policies" {}
 
-# Get AWS Caller identity
-data "aws_caller_identity" "current" {}
+data "rhcs_versions" "all" {}
 
-# Create the main four account roles for ROSA install
-module "create_account_roles" {
+module "create_account_roles"{
   source  = "terraform-redhat/rosa-sts/aws"
-  version = ">=0.0.5"
+  version = "0.0.12"
 
-  create_operator_roles = false
-  create_oidc_provider  = false
-  create_account_roles  = true
+  create_account_roles = true
 
-  account_role_prefix    = var.account_role_prefix
-  ocm_environment        = var.ocm_environment
-  rosa_openshift_version = var.openshift_version
-  account_role_policies  = data.ocm_policies.all_policies.account_role_policies
-  operator_role_policies = data.ocm_policies.all_policies.operator_role_policies
-  # Add permissions boundary here
+  account_role_prefix      = var.account_role_prefix
+  path                     = var.path
+  ocm_environment          = var.ocm_environment
+  rosa_openshift_version   = var.rosa_openshift_version
+  account_role_policies    = var.account_role_policies
+  all_versions             = var.all_versions
+  operator_role_policies   = var.operator_role_policies
+  tags                     = var.additional_tags
+
 }
 
-# Generate config for the OIDC provider that will be uploaded to the bucket
-resource "ocm_rosa_oidc_config_input" "oidc_input" {
-  region = var.region_name
+# Generates the OIDC config resources' names
+resource "rhcs_rosa_oidc_config_input" "oidc_input" {
+  count = var.managed ? 0 : 1
+  region = var.aws_region
 }
 
-# Generate the bucket that is the backend for the OIDC provider
+# Create the OIDC config resources on AWS
 module "oidc_config_input_resources" {
   source  = "terraform-redhat/rosa-sts/aws"
-  version = "0.0.5"
+  version = "0.0.14"
 
   create_oidc_config_resources = true
 
-  bucket_name             = ocm_rosa_oidc_config_input.oidc_input.bucket_name
-  discovery_doc           = ocm_rosa_oidc_config_input.oidc_input.discovery_doc
-  jwks                    = ocm_rosa_oidc_config_input.oidc_input.jwks
-  private_key             = ocm_rosa_oidc_config_input.oidc_input.private_key
-  private_key_file_name   = ocm_rosa_oidc_config_input.oidc_input.private_key_file_name
-  private_key_secret_name = ocm_rosa_oidc_config_input.oidc_input.private_key_secret_name
+  bucket_name             = one(rhcs_rosa_oidc_config_input.oidc_input[*].bucket_name)
+  discovery_doc           = one(rhcs_rosa_oidc_config_input.oidc_input[*].discovery_doc)
+  jwks                    = one(rhcs_rosa_oidc_config_input.oidc_input[*].jwks)
+  private_key             = one(rhcs_rosa_oidc_config_input.oidc_input[*].private_key)
+  private_key_file_name   = one(rhcs_rosa_oidc_config_input.oidc_input[*].private_key_file_name)
+  private_key_secret_name = one(rhcs_rosa_oidc_config_input.oidc_input[*].private_key_secret_name)
 }
 
-# Create the oidc provier config
-resource "ocm_rosa_oidc_config" "oidc_config" {
-  managed            = false
-  secret_arn         = module.oidc_config_input_resources.secret_arn
-  issuer_url         = ocm_rosa_oidc_config_input.oidc_input.issuer_url
-  installer_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.account_role_prefix}-Installer-Role"
-  depends_on         = [module.create_account_roles]
+resource "rhcs_rosa_oidc_config" "oidc_config" {
+  managed            = true
+  secret_arn         = one(module.oidc_config_input_resources[*].secret_arn)
+  issuer_url         = one(rhcs_rosa_oidc_config_input.oidc_input[*].issuer_url)
+  installer_role_arn = var.installer_role_arn
 }
 
-# Get information on the operator roles
-data "ocm_rosa_operator_roles" "operator_roles" {
+data "rhcs_rosa_operator_roles" "operator_roles" {
   operator_role_prefix = var.operator_role_prefix
   account_role_prefix  = var.account_role_prefix
 }
 
-# Generate the operator roles and OIDC provider. The operator roles rely on the OIDC provider and the provider relies on the bucket created above
 module "operator_roles_and_oidc_provider" {
   source  = "terraform-redhat/rosa-sts/aws"
-  version = ">=0.0.5"
+  version = "0.0.14"
 
   create_operator_roles = true
   create_oidc_provider  = true
 
-  cluster_id                  = var.cluster_name
-  rh_oidc_provider_thumbprint = ocm_rosa_oidc_config.oidc_config.thumbprint
-  rh_oidc_provider_url        = ocm_rosa_oidc_config.oidc_config.oidc_endpoint_url
-  operator_roles_properties   = data.ocm_rosa_operator_roles.operator_roles.operator_iam_roles
-  depends_on                  = [ocm_rosa_oidc_config.oidc_config]
+  cluster_id                  = ""
+  rh_oidc_provider_thumbprint = rhcs_rosa_oidc_config.oidc_config.thumbprint
+  rh_oidc_provider_url        = rhcs_rosa_oidc_config.oidc_config.oidc_endpoint_url
+  operator_roles_properties   = data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles
+  tags                        = var.additional_tags
+  path                        = var.path
 }
-
